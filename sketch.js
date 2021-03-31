0dd033f649c6d44008e82f4ff9edb20d10c5e51f
purisai <75816325+purisai@users.noreply.github.com>
2021-03-31 16:51:45 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,7 +52,7 @@ function setup() {
  	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
  	boxBase.shapeColor=color(255,0,0);
 
- 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:false} );
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
  	World.add(world, boxBottomBody);
 
  	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
